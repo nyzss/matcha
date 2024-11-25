4b417689d821e636270dabe19aac3e06f94a463c
Okan Koca <oknakoca@gmail.com>
2024-11-25 08:36:58 +0100

    initial layout is set

--- client/src/components/layout/app-layout.tsx
+++ client/src/components/layout/app-layout.tsx
@@ -1,18 +1,28 @@
 "use client";
 
-import { AppShell, Burger, Group, Skeleton } from "@mantine/core";
+import { AppShell, Burger, Button, Flex, Group } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
     const [opened, { toggle }] = useDisclosure();
 
+    const navbarAsideWidth = { base: 200, md: 200, lg: 400, xl: 550 };
+
     return (
         <AppShell
-            header={{ height: { base: 60, md: 70, lg: 80 } }}
+            header={{
+                height: { base: 60, md: 60, lg: 60 },
+            }}
+            footer={{ height: { base: 60, md: 70, lg: 80 } }}
             navbar={{
-                width: { base: 200, md: 200, lg: 300 },
+                width: navbarAsideWidth,
                 breakpoint: "sm",
                 collapsed: { mobile: !opened },
+            }}
+            aside={{
+                width: navbarAsideWidth,
+                breakpoint: "sm",
+                collapsed: { desktop: false, mobile: true },
             }}
             padding="md"
         >
@@ -24,18 +34,36 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
                         hiddenFrom="sm"
                         size="sm"
                     />
-                    <h1>Matcha</h1>
+                    {/* <h1>Matcha</h1> */}
                 </Group>
             </AppShell.Header>
             <AppShell.Navbar p="md">
-                Navbar
-                {Array(15)
-                    .fill(0)
-                    .map((_, index) => (
-                        <Skeleton key={index} animate={true} mt="sm" h={28} />
-                    ))}
+                <Flex
+                    direction={"column"}
+                    align={{
+                        base: "center",
+                        md: "flex-end",
+                    }}
+                    justify={"flex"}
+                    gap={"md"}
+                >
+                    <Button variant="subtle" size="lg">
+                        Home
+                    </Button>
+                    <Button variant="subtle" size="lg">
+                        Matches
+                    </Button>
+                    <Button variant="subtle" size="lg">
+                        Messages
+                    </Button>
+                    <Button variant="subtle" size="lg">
+                        Profile
+                    </Button>
+                </Flex>
             </AppShell.Navbar>
             <AppShell.Main>{children}</AppShell.Main>
+            <AppShell.Aside p="md">Aside</AppShell.Aside>
+            <AppShell.Footer p="md">Footer</AppShell.Footer>
         </AppShell>
     );
 }
