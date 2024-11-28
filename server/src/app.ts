bfc9017e14a08c8d3178ab0970f9f331ab0a97c9
Thomas <wshyatoto@gmail.com>
2024-11-28 12:30:38 +0100

    Update: Projet Structure

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,12 @@
 import fastify, {FastifyError, FastifyReply, FastifyRequest} from 'fastify';
 import routes from './app/routes';
 import plugins from "./app/plugins";
-import errorHandler from "./app/schemas/errorHandler";
+import errorHandler from "./app/utils/errorHandler";
 import { serializerCompiler, validatorCompiler, hasZodFastifySchemaValidationErrors } from "fastify-type-provider-zod";
 
 
 const buildApp = async () => {
-    const app = fastify({ logger: false });
+    const app = fastify({ logger: true });
 
     app.setValidatorCompiler(validatorCompiler);
     app.setSerializerCompiler(serializerCompiler);
