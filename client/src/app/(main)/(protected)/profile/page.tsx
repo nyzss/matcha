5df8ea43118d5afe81e4a6406cd28cd4034a1d77
Okan Koca <oknakoca@gmail.com>
2024-12-15 18:58:51 +0100

    init edit profile modal

--- client/src/app/(main)/(protected)/profile/page.tsx
+++ client/src/app/(main)/(protected)/profile/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { useAuthStore } from "@/lib/store";
 import { Profile } from "@/types/auth";
-import { Avatar, Box, Card, Image, Text } from "@mantine/core";
+import { Avatar, Box, Button, Card, Flex, Image, Modal, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import React from "react";
 
 export default function ProfilePage() {
     const user: Profile | null = useAuthStore((state) => state.user);
 
+    const [opened, { open, close }] = useDisclosure();
+    
     return (
         <Box h={"100vh"}>
             <Card>
@@ -21,14 +24,26 @@ export default function ProfilePage() {
                         fit="cover"
                     />
                 </Card.Section>
+                <Flex justify={"center"} align={"center"}>
+                <Flex direction={"column"}>
                 <Avatar
                     color="initials"
                     name={`${user?.firstName} ${user?.lastName}`}
                     size={100}
+                    mt={8}
                 ></Avatar>
-                <Text size="xl" fw={"bold"}>
+                <Text size="xl" fw={"bold"} mt={4}>
                     {user?.firstName} {user?.lastName} (@{user?.username})
                 </Text>
+                <Text>
+                    {user?.biography}
+                </Text>
+                </Flex>
+                <Modal opened={opened} onClose={close} title="Edit Profile" size={"xl"} centered>
+                    <h1>edit your profile content here</h1>
+                </Modal>
+                <Button ml={"auto"} variant="light" onClick={open}>Edit Profile</Button>
+                </Flex>
             </Card>
         </Box>
     );
